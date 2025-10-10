487bf05fa91b746c0f2151417094b3f4efdd13dc
Steelbash <105321479+Steelbash@users.noreply.github.com>
2025-10-10 16:21:21 +0300

    Add files via upload

--- RestBar/RestBar.lua
+++ RestBar/RestBar.lua
@@ -1,34 +1,23 @@
 local frame = CreateFrame('FRAME', 'RestBarFrame');
 
-local mouseOverCheckPeriod = 0.1
-local mouseOverLastCheckTime = 0.0
+local rbUpdatePeriod = 0.1      -- update bar period
+local rbLastUpdateTime = 0.0
 
 local uiStatusBar = nil
 local lastRestValue = 0
 
-local tickShowTime = 0
-local tickLifeTime = 2
+local isStartResting = false
+local tickRestStartTime = 0
+local amountRestOverTime = 0
 
+local tickRestCalcTime = 5       -- calc rest period
 
-function RB_SetPosition(msg)
-	if msg == "top" then
-		RT_STATUS_BAR_POS = "top"
-		frame:CreateStatusBar()
-		frame:UpdateRestBar()
-	end
-	
-	if msg == "bottom" then
-		RT_STATUS_BAR_POS = "bottom"
-		frame:CreateStatusBar()
-		frame:UpdateRestBar()
-	end
-end
 
 function frame:CreateStatusBar()
     if uiStatusBar ~= nil then
-	    uiStatusBar:Hide()
-	end
-
+		uiStatusBar:Hide()
+    end
+    
 	uiStatusBar = CreateFrame("StatusBar", nil, PlayerFrame, "TextStatusBar")
 	uiStatusBar:SetWidth(100)
 	uiStatusBar:SetHeight(12)
@@ -47,33 +36,38 @@ function frame:CreateStatusBar()
 	bd:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
 	uiStatusBar.bd = bd
 
-    local text = uiStatusBar:CreateFontString(nil, "OVERLAY")
+	local text = uiStatusBar:CreateFontString(nil, "OVERLAY")
 	text:SetPoint("CENTER", 0, 0)
 	text:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
 	uiStatusBar.text = text
 	
-	local tick = uiStatusBar:CreateFontString(nil, "OVERLAY")
-	tick:SetPoint("LEFT", 110, 0)
-	tick:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-	tick:SetTextColor(205, 0, 205, 1)
-	uiStatusBar.tick = tick
+	local tents = uiStatusBar:CreateFontString(nil, "OVERLAY", "GameFontGreen")
+	tents:SetPoint("LEFT", 105, 0)
+	tents:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+	uiStatusBar.tents = tents
+	
+	
+	local timeLeft = uiStatusBar:CreateFontString(nil, "OVERLAY")
+	timeLeft:SetPoint("RIGHT", uiStatusBar, "RIGHT", -5, 0)
+	timeLeft:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
+	uiStatusBar.timeLeft = timeLeft
 
-    uiStatusBar:ClearAllPoints()
-    
-    if RT_STATUS_BAR_POS == nil then	
-        RT_STATUS_BAR_POS = "top"
-    end
+	uiStatusBar:ClearAllPoints()
+	
+	if RT_STATUS_BAR_POS == nil then	
+		RT_STATUS_BAR_POS = "top"
+	end
 
 	if RT_STATUS_BAR_POS == "top" then
-        uiStatusBar:SetPoint("TOPLEFT", 114, -10)
-        uiStatusBar.bd:SetPoint("TOPLEFT", -10, 4)
-	    uiStatusBar.bd:SetTexCoord(0.0234375, 0.6875, 0.0, 1.0)
+		uiStatusBar:SetPoint("TOPLEFT", 114, -10)
+		uiStatusBar.bd:SetPoint("TOPLEFT", -10, 4)
+		uiStatusBar.bd:SetTexCoord(0.0234375, 0.6875, 0.0, 1.0)
 	end
 	
 	if RT_STATUS_BAR_POS == "bottom" then
-	    uiStatusBar:SetPoint("BOTTOMLEFT", 114, 23)
-	    uiStatusBar.bd:SetPoint("TOPLEFT", -12, 0)
-	    uiStatusBar.bd:SetTexCoord(0.0234375, 0.6875, 1.0, 0.0)
+		uiStatusBar:SetPoint("BOTTOMLEFT", 114, 23)
+		uiStatusBar.bd:SetPoint("TOPLEFT", -12, 0)
+		uiStatusBar.bd:SetTexCoord(0.0234375, 0.6875, 1.0, 0.0)
 	end
 	
 	uiStatusBar:SetValue(0)
@@ -81,87 +75,117 @@ function frame:CreateStatusBar()
 end
 
 function frame:UpdateRestBar()
-	local p = "player"
-	local x = UnitXP(p)
-	local m = UnitXPMax(p)
+	local x = UnitXP("player")
 	local r = GetXPExhaustion()
+	local maxRestValue = UnitXPMax("player") * 1.5
+	local tentsCount = 0
+	local timeForFullRest = 0
 	
+	if (UnitLevel("player") == 60) then
+		if uiStatusBar:IsVisible() then
+			uiStatusBar:Hide()
+		end
+		return
+    end
+		
 	if r == nil then 
 		r = 0
     end
     
-    if lastRestValue == nil or lastRestValue == 0 then
+    if lastRestValue == 0 then
 		lastRestValue = r
     end
-
-	local maxValue = m * 1.5
-	
-	uiStatusBar:SetMinMaxValues(0, maxValue)
-	uiStatusBar:SetValue(r)
-	
-	local diffValue = r - lastRestValue
+    
+    local diffValue = r - lastRestValue
 	lastRestValue = r
+
 	
-	if diffValue ~= 0 then
-		tickShowTime = GetTime()
-		if diffValue > 0 then
-		    uiStatusBar.tick:SetText("+"..diffValue.." rest")
-		else
-		    uiStatusBar.tick:SetText(diffValue.." rest")
-		end
-		uiStatusBar.tick:Show()
+	if IsResting() == nil and isStartResting then
+		isStartResting = false
+		uiStatusBar.tents:Hide()
 	end
 	
-	if mouseOverFlag then
+	if IsResting() ~= nil then
+		if not isStartResting then
+			isStartResting = true
+			tickRestStartTime = GetTime()
+			amountRestOverTime = 0
+		end
+	
+		amountRestOverTime = amountRestOverTime + diffValue
+
+		if (GetTime() - tickRestStartTime > tickRestCalcTime) then
+			tickRestStartTime = GetTime()
+			
+			if (amountRestOverTime > 0) then
+				local restInSecond = amountRestOverTime/tickRestCalcTime
+				tentsCount = math.floor(restInSecond/(maxRestValue*0.001) + 0.5)
+				timeForFullRest = (maxRestValue - r) / (tentsCount * (maxRestValue*0.001))
+
+				if tentsCount > 0 then
+					uiStatusBar.tents:SetText("x"..tostring(tentsCount))
+					uiStatusBar.tents:Show()
+					
+					uiStatusBar.timeLeft:SetText(format("%dm",floor(timeForFullRest/60)))
+					uiStatusBar.timeLeft:Show()
+				end
+
+				amountRestOverTime = 0
+			end
+			
+			if tentsCount == 0 then
+				uiStatusBar.tents:Hide()
+				uiStatusBar.timeLeft:Hide()
+			end
+		end
+	else
+		uiStatusBar.tents:Hide()
+		uiStatusBar.timeLeft:Hide()
+	end
+
+
+	uiStatusBar:SetMinMaxValues(0, maxRestValue)
+	uiStatusBar:SetValue(r)
+
+	if MouseIsOver(uiStatusBar) then
 		uiStatusBar.text:SetText(r)
     else
-        uiStatusBar.text:SetText(math.floor(r/maxValue*100).."%")
+		uiStatusBar.text:SetText(math.floor(r/maxRestValue*100).."%")
     end
 end
 
+
 frame:SetScript("OnUpdate", function()
-    if GetTime() - tickShowTime > tickLifeTime then
-        uiStatusBar.tick:Hide()
-    end
-    
-    if GetTime() - mouseOverLastCheckTime > mouseOverCheckPeriod then
-        mouseOverLastCheckTime = GetTime()
-        
-        if mouseOverFlag ~= MouseIsOver(uiStatusBar) then
-		    mouseOverFlag = MouseIsOver(uiStatusBar);
-		    frame:UpdateRestBar()
-		end
+	if GetTime() - rbLastUpdateTime > rbUpdatePeriod then
+        rbLastUpdateTime = GetTime()
+        frame:UpdateRestBar()
 	end
 end)
 
-function frame:ADDON_LOADED()
-end
-
-function frame:UPDATE_EXHAUSTION()
-	frame:UpdateRestBar()
-end
-
-function frame:PLAYER_UPDATE_RESTING()
-    frame:UpdateRestBar()
-end
-
-function frame:PLAYER_ENTERING_WORLD()
+function frame:VARIABLES_LOADED()
 	frame:CreateStatusBar()
-    frame:UpdateRestBar()
 end
 
-
-------------------------------------------------------------------------
 
 frame:SetScript('OnEvent', function()
 	this[event]()
 end)
 
-frame:RegisterEvent("ADDON_LOADED")
-frame:RegisterEvent("UPDATE_EXHAUSTION")
-frame:RegisterEvent("PLAYER_UPDATE_RESTING")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+function RB_SetPosition(msg)
+	if msg == "top" then
+		RT_STATUS_BAR_POS = "top"
+		frame:CreateStatusBar()
+		frame:UpdateRestBar()
+	end
+	
+	if msg == "bottom" then
+		RT_STATUS_BAR_POS = "bottom"
+		frame:CreateStatusBar()
+		frame:UpdateRestBar()
+	end
+end
 
+frame:RegisterEvent("VARIABLES_LOADED")
 
 SLASH_RESTBAR1 = "/restbar"
 SlashCmdList["RESTBAR"] = RB_SetPosition
